93678c2f1b588cb56d93bb85ade3ed3bd118ad58
osh.silva <osh.silva@gmail.com>
2024-04-19 16:52:53 +0530

    Add industry create and update APIS

--- src/views/industry/components/IndustryModal/index.js
+++ src/views/industry/components/IndustryModal/index.js
@@ -15,6 +15,49 @@ export default function IndustryModal(props) {
     const createIndustry = (payload) => {
         console.log('payload');
         console.log(payload);
+        if (props.action === 'create') {
+            fetch('https://kms-backend.azurewebsites.net/api/industry', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
+        } else {
+            const url = `https://kms-backend.azurewebsites.net/api/industry?id=${payload.id}`;
+
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
+        }
     }
 
     console.log('--props----');
