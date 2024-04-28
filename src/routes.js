bdbf903887ba34ced3b7f454a1a5be3262e2d71a
osh.silva <osh.silva@gmail.com>
2024-04-28 15:31:19 +0530

    Show only v1 changes in nav bar

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ import {
 } from "react-icons/md";
 
 // Admin Imports
-import Employee from "./views/employee";
-import EmployeeType from "views/employee-type";
+// import Employee from "./views/employee";
+// import EmployeeType from "views/employee-type";
 import Industry from "./views/industry";
 import Client from "./views/client";
-import ProjectCategory from "./views/project-category";
-import Project from "./views/project";
+// import ProjectCategory from "./views/project-category";
+// import Project from "./views/project";
 
 const routes = [
   {
@@ -28,34 +28,34 @@ const routes = [
     path: "/client",
     component: Client,
   },
-  {
-    name: "Project Category",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
-    path: "/project-category",
-    component: ProjectCategory,
-  },
-  {
-    name: "Project",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
-    path: "/project",
-    component: Project,
-  },
-  {
-    name: "Employees",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
-    path: "/data-tables",
-    component: Employee,
-  },
-  {
-    name: "Employee Types",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit'/>,
-    path: "/employee-types",
-    component: EmployeeType,
-  }
+  // {
+  //   name: "Project Category",
+  //   layout: "/admin",
+  //   icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+  //   path: "/project-category",
+  //   component: ProjectCategory,
+  // },
+  // {
+  //   name: "Project",
+  //   layout: "/admin",
+  //   icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+  //   path: "/project",
+  //   component: Project,
+  // },
+  // {
+  //   name: "Employees",
+  //   layout: "/admin",
+  //   icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+  //   path: "/data-tables",
+  //   component: Employee,
+  // },
+  // {
+  //   name: "Employee Types",
+  //   layout: "/admin",
+  //   icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit'/>,
+  //   path: "/employee-types",
+  //   component: EmployeeType,
+  // }
 ];
 
 export default routes;
